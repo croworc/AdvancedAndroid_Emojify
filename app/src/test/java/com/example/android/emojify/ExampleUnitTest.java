c1e9f3d8d7a5ed86a983539ea5a1541a9da6c779
ikrotzky <ikr@croworc.com>
2019-05-01 18:35:02 +0200

    [TLIB.06-Ex] fix: Broken unit test & instrumented test
    Was broken due to upgrade to androidx libraries

--- app/src/test/java/com/example/android/emojify/ExampleUnitTest.java
+++ app/src/test/java/com/example/android/emojify/ExampleUnitTest.java
@@ -1,8 +1,7 @@
 package com.example.android.emojify;
 
 import org.junit.Test;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +10,7 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
+    public void addition_isCorrect() throws AssertionError {
         assertEquals(4, 2 + 2);
     }
 }
